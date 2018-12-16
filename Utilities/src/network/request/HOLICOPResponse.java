6ef540a3b9fc5e13bbe1a5758deb7420b6790e0d
Jonathan C <32371525+jona-1993@users.noreply.github.com>
2018-12-16 09:50:44 +0100

    Eval 4-5

---
+++ Utilities/src/network/request/HOLICOPResponse.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package network.request;
+
+import java.io.Serializable;
+import network.request.interfaces.Reponse;
+
+/**
+ *
+ * @author jona1993
+ */
+public class FUCAMPResponse implements Reponse, Serializable {
+    public final static int OK = 1;
+    public final static int NOK = 2;
+    public final static int ERROR = -1; 
+    
+    private int codeRetour;
+    private String chargeUtile;
+    public String cmd;
+    
+    public FUCAMPResponse(int codeRetour, String chargeUtile, String cmd) {
+        this.codeRetour = codeRetour;
+        this.chargeUtile = chargeUtile;
+        this.cmd = cmd;
+    }
+
+    @Override
+    public int getCode() {
+        return codeRetour;
+    }
+
+    public String getChargeUtile() {
+        return chargeUtile;
+    }
+
+    public void setChargeUtile(String chargeUtile) {
+        this.chargeUtile = chargeUtile;
+    }
+    
+    
+    
+}
